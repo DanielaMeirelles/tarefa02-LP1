3d007d538c573deeb540d76f6ef1f4d7dd074b5d
DanielaMeirelles <daniela.meirelles90@outlook.com>
2025-05-15 20:37:18 -0300

    refactor Main and Fracao

--- src/main/java/org/example/Fracao.java
+++ src/main/java/org/example/Fracao.java
@@ -1,21 +1,52 @@
 package org.example;
 
 public class Fracao {
+    int numerador;
+    int denominador;
 
     public Fracao(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+        simplificar();
     }
 
+    public Fracao somar(Fracao outra) {
+        int num = this.numerador * outra.denominador + outra.numerador * this.denominador;
+        int den = this.denominador * outra.denominador;
+        return new Fracao(num, den);
     }
 
+    public Fracao subtrair(Fracao outra) {
+        int num = this.numerador * outra.denominador - outra.numerador * this.denominador;
+        int den = this.denominador * outra.denominador;
+        return new Fracao(num, den);
     }
 
+    public Fracao multiplicar(Fracao outra) {
+        int num = this.numerador * outra.numerador;
+        int den = this.denominador * outra.denominador;
+        return new Fracao(num, den);
     }
 
+    public Fracao dividir(Fracao outra) {
+        int num = this.numerador * outra.denominador;
+        int den = this.denominador * outra.numerador;
+        return new Fracao(num, den);
     }
 
+    private void simplificar() {
+        int mdc = calcularMDC(numerador, denominador);
+        numerador /= mdc;
+        denominador /= mdc;
     }
 
+    private int calcularMDC(int a, int b) {
+        if (b == 0) return a;
+        return calcularMDC(b, a % b);
     }
+
+    public void exibir() {
+        System.out.println(numerador + "/" + denominador);
 
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,54 +7,109 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Criando objetos iniciais
+        Animal animal= new Animal("Tigre", "Felino", 7);
+        Bicicleta bicicleta = new Bicicleta("Caloi 100", "Azul", 21);
+        Carro carro = new Carro("Civic", "Prata", 0);
+        Celular celular = new Celular("Samsung", "Galaxy S23", 256);
+        Computador computador = new Computador("Dell", "Intel i7", 16);
+        Filme filme = new Filme("Matrix", "Ficção Científica", 136);
+        Livro livro = new Livro("Dom Casmurro", "Machado de Assis", 256);
+        Produto produto = new Produto("Mouse gamer", 149.90, 5);
         Pessoa pessoa = new Pessoa("Carlos", 30, "123.456.789-00");
         Flor flor = new Flor("Rosa", "Vermelha", 0.5f);
 
         int opcao;
         do {
-            System.out.println("\n===============MENU=============");
+            System.out.println("\n========MENU PRINCIPAL=============");
             System.out.println("Escolha uma das ações abaixo e digite o número correspondente.");
-            System.out.println("1- Fazer a pessoa falar");
-            System.out.println("2- Fazer a pessoa andar");
-            System.out.println("3- Fazer a pessoa dormir");
-            System.out.println("4- Fazer a flor florescer");
-            System.out.println("5- Fazer a flor crescer");
-            System.out.println("6- Fazer a flor murchar");
-            System.out.println("7- Sair");
+            System.out.println("1. Testar Pessoa");
+            System.out.println("2. Testar Flor");
+            System.out.println("3. Testar Carro");
+            System.out.println("4. Testar Animal");
+            System.out.println("5. Testar Celular");
+            System.out.println("6. Testar Livro");
+            System.out.println("7. Testar Computador");
+            System.out.println("8. Testar Bicicleta");
+            System.out.println("9. Testar Filme");
+            System.out.println("10. Testar Produto");
+            System.out.println("11. Testar Frações");
+            System.out.println("0. Sair");
             System.out.print("Escolha uma opção: ");
-
             opcao = scanner.nextInt();
             scanner.nextLine();
 
             switch (opcao) {
                 case 1:
                     pessoa.falar();
-                    break;
-                case 2:
                     pessoa.andar();
                     break;
-                case 3:
-                    pessoa.dormir();
-                    break;
-                case 4:
+                case 2:
+                    flor.crescer(1.5f);
                     flor.florescer();
                     break;
+                case 3:
+                    carro.exibirDados();
+                    carro.acelerar();
+                    break;
+                case 4:
+                    animal.mostrarInfo();
+                    animal.emitirSom();
+                    break;
                 case 5:
-                    System.out.println("Digite quanto a flor deve crescer (em centímetros): ");
-                    double crescimento = scanner.nextDouble();
-                    flor.crescer(crescimento);
+                    celular.exibirInfo();
+                    celular.ligar();
                     break;
                 case 6:
-                    flor.murchar();
+                    livro.mostrarDetalhes();
+                    livro.abrir();
                     break;
                 case 7:
+                    computador.infoSistema();
+                    computador.ligar();
+                    break;
+                case 8:
+                    bicicleta.detalhes();
+                    bicicleta.pedalar();
+                    break;
+                case 9:
+                    filme.exibirInfo();
+                    filme.reproduzir();
+                    break;
+                case 10:
+                    produto.exibirProduto();
+                    produto.vender();
+                    break;
+                case 11:
+                    System.out.print("Digite o numerador da primeira fração: ");
+                    int num1 = scanner.nextInt();
+                    System.out.print("Digite o denominador da primeira fração: ");
+                    int den1 = scanner.nextInt();
+
+                    System.out.print("Digite o numerador da segunda fração: ");
+                    int num2 = scanner.nextInt();
+                    System.out.print("Digite o denominador da segunda fração: ");
+                    int den2 = scanner.nextInt();
+
+                    Fracao fr1 = new Fracao(num1, den1);
+                    Fracao fr2 = new Fracao(num2, den2);
+
+                    Fracao soma = fr1.somar(fr2);
+                    System.out.print("Soma das frações: ");
+                    soma.exibir();
+                    Fracao mult = fr1.multiplicar(fr2);
+                    System.out.print("Multiplicação das frações: ");
+                    mult.exibir();
+                    break;
+                case 0:
+                    System.out.println("Saindo do programa.");
                     System.out.println("Saindo... Até mais!");
+                    scanner.close();
                     break;
                 default:
                     System.out.println("Opção inválida! Tente novamente.");
             }
-        } while (opcao != 9);
 
+        } while (opcao != 0);
         scanner.close();
     }
 }
